fcedf3930b2dd744e268a5360172e7751142ba37
auniafrinaa06-sketch <auniafrinaa06@gmail.com>
2026-09-21 17:42:31 +0800

    Update api.py

--- api.py
+++ api.py
@@ -16,24 +16,24 @@ DB_CONFIG = {
 def get_db_connection():
     return mysql.connector.connect(**DB_CONFIG)
 
-# =========================================================
-# API UNTUK SIMPAN LOG AKSES DARI ESP32
-# =========================================================
 @app.route('/api/log', methods=['POST'])
 def insert_log():
     try:
-        # Ambil terus data yang dihantar dari ESP32
-        uid_card = request.form.get('uid_card', 'UNKNOWN_UID')
-        status = request.form.get('status', 'SUCCESS')
-        username = request.form.get('username', 'Pelawat')
-
-        if not uid_card:
-            return "MISSING_PARAMS", 400
+        # Terima data dari form submission mahupun JSON submission
+        if request.is_json:
+            data = request.get_json()
+            uid_card = data.get('uid_card', 'UNKNOWN_UID')
+            status = data.get('status', 'FAILED')
+            username = data.get('username', 'Pelawat')
+        else:
+            uid_card = request.form.get('uid_card', 'UNKNOWN_UID')
+            status = request.form.get('status', 'FAILED')
+            username = request.form.get('username', 'Pelawat')
 
         conn = get_db_connection()
         cursor = conn.cursor()
 
-        # Direct INSERT ke dalam access_log tanpa semak jadual registered_users
+        # Insert terus log ke MySQL
         log_query = "INSERT INTO access_log (uid_card, username, status) VALUES (%s, %s, %s)"
         cursor.execute(log_query, (uid_card, username, status))
         conn.commit()
@@ -47,9 +47,9 @@ def insert_log():
         }), 200
 
     except Exception as e:
+        print("Error:", str(e))
         return f"ERROR: {str(e)}", 500
 
-# Main route untuk semak status API
 @app.route('/')
 def home():
     return "Smart Door Access API Online!"
